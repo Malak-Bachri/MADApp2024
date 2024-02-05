1c894fd535f33549a62c294b6b008b171feac414
Malak-Bachri <malakbachri145@gmail.com>
2024-02-05 09:15:06 -0600

    Hi

---
+++ app/src/main/java/com/example/bignumber/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.bignumber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    private var leftNum :Int = 0;
+    private var rightNum :Int = 0;
+    private var score :Int = 0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        pickRandomNumber()
+    }
+
+    fun leftButtonOnClick(view: View)
+    {
+        pickRandomNumber()
+    }
+
+
+    fun rightButtonOnClick(view: View)
+    {
+        pickRandomNumber()
+    }
+
+
+    fun pickRandomNumber()
+    {
+        var leftButton = findViewById<Button>(R.id.left_number_button);
+        var rightButton = findViewById<Button>(R.id.right_number_button);
+
+        var rand = Random()
+
+        leftNum =  rand.nextInt(10);
+        rightNum =  rand.nextInt(10);
+
+        leftButton.text = "$leftNum";
+        rightButton.text = "$rightNum";
+    }
+}
